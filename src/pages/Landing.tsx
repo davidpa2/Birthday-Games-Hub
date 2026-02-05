6ae7ffd13649c29047d6626ec895b4a9efe580a0
David Padilla <davidpaag2002@gmail.com>
2026-02-05 22:58:22 +0100

    feat(landing): Improved landing card style

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -19,8 +19,8 @@ function Landing() {
                     gyroControls: false,
                     minHeight: 200.0,
                     minWidth: 200.0,
-                    sunlightColor: 'fff',
-                    sunGlareColor: 'fff',
+                    sunlightColor: '0xffffff',
+                    sunGlareColor: '0xffffff',
                 }),
             );
         }
@@ -41,9 +41,9 @@ function Landing() {
                 color: 'white',
                 zIndex: 1
             }}>
-                <div className="w-96 p-6 border-3 rounded-xl shadow-xs bg-gray-800/25 text-center">
-                    <svg className="w-7 h-7 mb-3 flex-row content-center" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 21v-9m3-4H7.5a2.5 2.5 0 1 1 0-5c1.5 0 2.875 1.25 3.875 2.5M14 21v-9m-9 0h14v8a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-8ZM4 8h16a1 1 0 0 1 1 1v3H3V9a1 1 0 0 1 1-1Zm12.155-5c-3 0-5.5 5-5.5 5h5.5a2.5 2.5 0 0 0 0-5Z" />
+                <div className="w-96 p-6 border-3 rounded-xl shadow-xs bg-gray-800/25">
+                    <svg className="w-7 h-7 mb-3 flex-row left-1/2 right-1/2 mx-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 21v-9m3-4H7.5a2.5 2.5 0 1 1 0-5c1.5 0 2.875 1.25 3.875 2.5M14 21v-9m-9 0h14v8a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-8ZM4 8h16a1 1 0 0 1 1 1v3H3V9a1 1 0 0 1 1-1Zm12.155-5c-3 0-5.5 5-5.5 5h5.5a2.5 2.5 0 0 0 0-5Z" />
                     </svg>
 
                     <a href="#">
@@ -55,7 +55,7 @@ function Landing() {
 
                     <a href="#" className="inline-flex font-medium items-center text-fg-brand hover:underline">
                         ¡A jugar!
-                        <svg className="w-4 h-4 ms-2 rtl:rotate-270" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" /></svg>
+                        <svg className="w-4 h-4 ms-2 rtl:rotate-270" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" /></svg>
                     </a>
                 </div>
             </div>
