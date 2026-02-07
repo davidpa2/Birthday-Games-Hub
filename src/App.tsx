3e214862fcfe1f4cd7e58fd7cc5701333dbeac5b
David Padilla <davidpaag2002@gmail.com>
2026-02-07 18:13:57 +0100

    feat(games): Navigation to Games page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
+import Games from './pages/Games';
 
 function App() {
   return (
+    <>
     <Routes>
       <Route path="/" element={<Landing/>} />
     </Routes>
+    
+    <Routes>
+      <Route path="/Games" element={<Games/>} />
+    </Routes>
+    </>
   );
 }
 
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import * as THREE from "three"; // Vanta suele necesitar Three.js
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
@@ -53,10 +54,12 @@ function Landing() {
                     <p className="mb-3">¡Bienvenido al portal de juegos de cumpleaños!</p>
                     <p className="mb-3">Todos los juegos sobre tartas hechos año tras año en un mismo lugar.</p>
 
-                    <a href="#" className="inline-flex font-medium items-center text-fg-brand hover:underline">
-                        ¡A jugar!
-                        <svg className="w-4 h-4 ms-2 rtl:rotate-270" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" /></svg>
-                    </a>
+                    <Link to={"/Games"}>
+                        <a className="inline-flex font-medium items-center text-fg-brand hover:underline">
+                            ¡A jugar!
+                            <svg className="w-4 h-4 ms-2 rtl:rotate-270" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" /></svg>
+                        </a>
+                    </Link>
                 </div>
             </div>
             }
