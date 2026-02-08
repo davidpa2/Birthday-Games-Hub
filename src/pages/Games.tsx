407700b50b542fffa761c399430e04307d3b0953
David Padilla <davidpaag2002@gmail.com>
2026-02-08 22:56:50 +0100

    feat(games): First card design with image

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -4,7 +4,12 @@ type Props = {}
 
 const Games = (props: Props) => {
   return (
-    <div>Games</div>
+    <div className="w-96 h-56 border-3 rounded-xl shadow-xs bg-gray-800/25" style={{backgroundImage: "url(" + "src/assets/36.png" + ")", backgroundPosition: 'center', backgroundSize: '100%'}}>
+      {/* <img className='11' src="src\assets\36.png" alt="" /> */}
+      <h1 className='text-red-500 text-3xl font-sans'>Cake Drive</h1>
+
+      <p className='text-white'>¡Conduce hasta entre obstáculos y a través de las curvas hasta alcanzar 36 tartas!</p>
+    </div>
   )
 }
 
