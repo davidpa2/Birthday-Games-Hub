2c896f7bebaa8b54faf14965247a9688b167b6eb
David Padilla <davidpaag2002@gmail.com>
2026-02-09 23:01:13 +0100

    feat(games): Added png photo to see card

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -4,7 +4,8 @@ type Props = {}
 
 const Games = (props: Props) => {
   return (
-    <div className="w-96 h-56 border-3 rounded-xl shadow-xs bg-gray-800/25" style={{backgroundImage: "url(" + "src/assets/36.png" + ")", backgroundPosition: 'center', backgroundSize: '100%'}}>
+    <div className="w-96 h-56 border-3 rounded-xl shadow-xs bg-gray-800/25"
+        style={{backgroundImage: "url(" + "src/assets/36.png" + ")", backgroundPosition: 'center', backgroundSize: '100%'}}>
       {/* <img className='11' src="src\assets\36.png" alt="" /> */}
       <h1 className='text-red-500 text-3xl font-sans'>Cake Drive</h1>
 
