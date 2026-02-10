f538bf827516b058edc00393da7e5d37d2446bc6
David Padilla <davidpaag2002@gmail.com>
2026-02-10 23:10:13 +0100

    feat(games): First right version of styled game card

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 
-type Props = {}
-
-const Games = (props: Props) => {
+const Games = () => {
   return (
-    <div className="w-96 h-56 border-3 rounded-xl shadow-xs bg-gray-800/25"
+    <div className="w-96 h-56 border-white border-3 rounded-xl shadow-xs bg-gray-800/5 flex flex-col justify-between"
         style={{backgroundImage: "url(" + "src/assets/36.png" + ")", backgroundPosition: 'center', backgroundSize: '100%'}}>
-      {/* <img className='11' src="src\assets\36.png" alt="" /> */}
-      <h1 className='text-red-500 text-3xl font-sans'>Cake Drive</h1>
+      <h1 className='text-red-500 text-5xl p-2 font-[Pangolin] font-bold stroke-3 drop-shadow-[0_3px_0px_rgba(255,255,255,1)]'>Cake Drive</h1>
 
-      <p className='text-white'>¡Conduce hasta entre obstáculos y a través de las curvas hasta alcanzar 36 tartas!</p>
+      <p className='text-white bg-gray-800/10 p-2'>¡Conduce hasta entre obstáculos y a través de las curvas hasta alcanzar 36 tartas!</p>
     </div>
   )
 }
