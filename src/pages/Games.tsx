8e3a0aec3f51e1611ef706fb9a81cd807bb6534c
David Padilla <davidpaag2002@gmail.com>
2026-02-19 22:53:31 +0100

    feat(games): Trying to show a floating cloud per card

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+import cloud from "../assets/cloud.png";
 import img34 from "../assets/34.png";
 import img35 from "../assets/35.png";
 import img36 from "../assets/36.png";
@@ -42,27 +43,31 @@ const Games = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-8 place-items-center my-20'>
       {
-        gamesArray.map(game => 
-          <div className="w-96 h-100 border-white border-3 rounded-xl shadow-xl bg-gray-800/5 flex flex-col items-center">
+        gamesArray.map(game =>
+          <>
+            <div className="w-96 h-100 border-white border-3 rounded-xl shadow-xl bg-gray-800/5 flex flex-col items-center">
+              <img src={cloud} alt="cloud image" className="cloud" />
 
-            <img src={game.image} alt="" className='w-80 mt-6' />
+              <img src={game.image} alt="" className='w-80 mt-6' />
 
-            <h1 className='mb-2 mt-4 text-2xl font-semibold tracking-tight text-heading text-white'>{game.name}</h1>
+              <h1 className='mb-2 mt-4 text-2xl font-semibold tracking-tight text-heading text-white'>{game.name}</h1>
 
-            <p className='text-white text-center p-2'>{game.description}</p>
+              <p className='text-white text-center p-2'>{game.description}</p>
 
-            <div className='w-full flex justify-center mt-3'>
-              <button className='text-center ring-3 text-white w-26 h-9' onClick={() => {  window.location.href=game.url; console.log("aaaaaaaa");
-              }}>
-                <div className='flex'>
-                  <svg className="w-7 h-7 mt-0.5 mx-1 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 21v-9m3-4H7.5a2.5 2.5 0 1 1 0-5c1.5 0 2.875 1.25 3.875 2.5M14 21v-9m-9 0h14v8a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-8ZM4 8h16a1 1 0 0 1 1 1v3H3V9a1 1 0 0 1 1-1Zm12.155-5c-3 0-5.5 5-5.5 5h5.5a2.5 2.5 0 0 0 0-5Z" />
-                  </svg>
-                  ¡Jugar!
-                </div>
-              </button>
+              <div className='w-full flex justify-center mt-3'>
+                <button className='text-center ring-3 text-white w-26 h-9' onClick={() => {
+                  window.location.href = game.url; console.log("aaaaaaaa");
+                }}>
+                  <div className='flex'>
+                    <svg className="w-7 h-7 mt-0.5 mx-1 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 21v-9m3-4H7.5a2.5 2.5 0 1 1 0-5c1.5 0 2.875 1.25 3.875 2.5M14 21v-9m-9 0h14v8a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-8ZM4 8h16a1 1 0 0 1 1 1v3H3V9a1 1 0 0 1 1-1Zm12.155-5c-3 0-5.5 5-5.5 5h5.5a2.5 2.5 0 0 0 0-5Z" />
+                    </svg>
+                    ¡Jugar!
+                  </div>
+                </button>
+              </div>
             </div>
-          </div>
+          </>
         )}
     </div>
   )
