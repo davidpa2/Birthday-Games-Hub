71084cdbf97b9090182e56cc50a1a4c0464447e2
David Padilla <davidpaag2002@gmail.com>
2026-02-27 23:15:51 +0100

    feat(game.-frame): Responsive game-frame layout

--- src/pages/Game-frame.tsx
+++ src/pages/Game-frame.tsx
@@ -1,15 +1,21 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { FaArrowLeft } from "react-icons/fa6";
 
 const GameFrame = () => {
     const location = useLocation();
+    const navigate = useNavigate();
     const { gameName, url } = location.state || { gameName: 'Cargando...', url: '' };
 
     return (
-        <div className="w-full h-screen">
-            <h1 className="text-center py-4 text-4xl">{gameName}</h1>
+        <div className="w-full h-screen flex flex-col items-center">
+            <header className='w-full sm:w-[80vh] md:w-[70vh] lg:w-[60vh] grid grid-cols-2'>
+                <FaArrowLeft className='text-4xl flex self-center justify-end cursor-pointer' onClick={() => navigate("/games")}/>
+                <h1 className="text-center py-4 text-4xl flex justify-end">{gameName}</h1>
+            </header>
             <iframe 
                 src={url} 
-                className="w-full h-[80vh]" 
+                id='frame'
+                className="w-full sm:w-[80vh] md:w-[70vh] lg:w-[60vh] h-[90vh]" 
                 frameBorder="0"
             ></iframe>
         </div>
