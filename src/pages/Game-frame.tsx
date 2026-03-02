a9ad8160c83ffb511192133262b0eee313b894d5
David Padilla <davidpaag2002@gmail.com>
2026-03-02 22:59:58 +0100

    feat(game-frame): Adjusted iframe to the bottom

--- src/pages/Game-frame.tsx
+++ src/pages/Game-frame.tsx
@@ -7,7 +7,7 @@ const GameFrame = () => {
     const { gameName, url } = location.state || { gameName: 'Cargando...', url: '' };
 
     return (
-        <div className="w-full h-screen flex flex-col items-center">
+        <div className="w-full h-screen flex flex-col items-center justify-end">
             <header className='w-full sm:w-[80vh] md:w-[70vh] lg:w-[60vh] grid grid-cols-[20%_80%]'>
                 <FaArrowLeft className='text-4xl ml-3 flex self-center justify-end cursor-pointer' onClick={() => navigate("/games")}/>
                 <h1 className="text-center mr-5 py-4 text-4xl flex justify-end">{gameName}</h1>
@@ -15,7 +15,7 @@ const GameFrame = () => {
             <iframe 
                 src={url} 
                 id='frame'
-                className="w-full sm:w-[80vh] md:w-[70vh] lg:w-[60vh] h-[90vh]" 
+                className="w-full sm:w-[80vh] md:w-[70vh] lg:w-[60vh] h-[95vh]" 
                 frameBorder="0"
             ></iframe>
         </div>
