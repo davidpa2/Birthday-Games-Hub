96e1945ea861e8232e62d9cc240032f8958f5c42
David Padilla <davidpaag2002@gmail.com>
2026-03-02 22:50:18 +0100

    feat(game-frame): Improved header layout

--- src/pages/Game-frame.tsx
+++ src/pages/Game-frame.tsx
@@ -8,9 +8,9 @@ const GameFrame = () => {
 
     return (
         <div className="w-full h-screen flex flex-col items-center">
-            <header className='w-full sm:w-[80vh] md:w-[70vh] lg:w-[60vh] grid grid-cols-2'>
-                <FaArrowLeft className='text-4xl flex self-center justify-end cursor-pointer' onClick={() => navigate("/games")}/>
-                <h1 className="text-center py-4 text-4xl flex justify-end">{gameName}</h1>
+            <header className='w-full sm:w-[80vh] md:w-[70vh] lg:w-[60vh] grid grid-cols-[20%_80%]'>
+                <FaArrowLeft className='text-4xl ml-3 flex self-center justify-end cursor-pointer' onClick={() => navigate("/games")}/>
+                <h1 className="text-center mr-5 py-4 text-4xl flex justify-end">{gameName}</h1>
             </header>
             <iframe 
                 src={url} 
